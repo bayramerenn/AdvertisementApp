2e2554d92430b8b7ba0f042f910156ef1fc4ae33
Bayram Eren <bayram.eren@yuchisoft.onmicrosoft.com>
2022-03-11 23:12:21 +0300

    Response Generic class added

---
+++ AdvertisementApp.Common/Response{T}.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Common
+{
+    internal class Response<T> : Response, IResponse<T>
+    {
+        public T Data { get; set; }
+        public List<CustomValidatorError> ValidatorErrors { get; set; }
+        public Response(ResponseType responseType,string message) : base(responseType,message)
+        {
+        }
+        public Response(ResponseType responseType,T data):base(responseType)
+        {
+            Data = data;
+        }
+        public Response(T data,List<CustomValidatorError> errors):base(ResponseType.ValidatorError)
+        {
+            Data=data;
+            ValidatorErrors = errors;   
+        }
+    }
+}
