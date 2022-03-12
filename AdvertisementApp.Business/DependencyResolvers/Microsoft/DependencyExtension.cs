e480ace2217e750b5411b3e485c195cf662f0892
Bayram Eren <bayram.eren@yuchisoft.onmicrosoft.com>
2022-03-12 21:07:27 +0300

    Validator added

--- AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
+++ AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -1,24 +1,21 @@
 ﻿using AdvertisementApp.Business.ValidationRules;
 using AdvertisementApp.DataAccess.Contexts;
+using AdvertisementApp.Dtos.AdvertisementAppUserDtos;
+using AdvertisementApp.Dtos.AdvertisementDtos;
+using AdvertisementApp.Dtos.AppUserDtos;
+using AdvertisementApp.Dtos.GenderDtos;
 using AdvertisementApp.Dtos.ProvidedServiceDtos;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdvertisementApp.Business.DependencyResolvers.Microsoft
 {
     public static class DependencyExtension
     {
-        public static void AddDependencies(this IServiceCollection services,IConfiguration configuration)
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
         {
-
-      
             services.AddDbContext<AdvertisementContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("Local"));
@@ -26,6 +23,20 @@ namespace AdvertisementApp.Business.DependencyResolvers.Microsoft
 
             services.AddTransient<IValidator<ProvidedServiceCreateDto>, ProvidedServiceCreateDtoValidator>();
             services.AddTransient<IValidator<ProvidedServiceUpdateDto>, ProvidedServiceUpdateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementCreateDto>, AdvertisementCreateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementUpdateDto>, AdvertisementUpdateDtoValidator>();
+            services.AddTransient<IValidator<AppUserCreateDto>, AppUserCreateDtoValidator>();
+            services.AddTransient<IValidator<AppUserUpdateDto>, AppUserUpdateDtoValidator>();
+            services.AddTransient<IValidator<AppUserLoginDto>, AppUserLoginDtoValidator>();
+            services.AddTransient<IValidator<GenderCreateDto>, GenderCreateDtoValidator>();
+            services.AddTransient<IValidator<GenderUpdateDto>, GenderUpdateDtoValidator>();
+            services.AddTransient<IValidator<AdvertisementAppUserCreateDto>, AdvertisementAppUserCreateDtoValidator>();
+
+            //services.AddScoped<IProvidedServiceService, ProvidedServiceService>();
+            //services.AddScoped<IAdvertisementService, AdvertisementService>();
+            //services.AddScoped<IAppUserService, AppUserService>();
+            //services.AddScoped<IGenderService, GenderService>();
+            //services.AddScoped<IAdvertisementAppUserService, AdvertisementAppUserService>();
         }
     }
 }
