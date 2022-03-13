a48560ff391938bfcf8ceb0c9e26e1d5bfdbd90f
Bayram Eren <bayram.eren@yuchisoft.onmicrosoft.com>
2022-03-13 17:46:42 +0300

    Account controller finished

--- AdvertisementApp.UI/Program.cs
+++ AdvertisementApp.UI/Program.cs
@@ -1,10 +1,28 @@
 using AdvertisementApp.Business.DependencyResolvers.Microsoft;
+using AdvertisementApp.UI.Mappings.AutoMapper;
+using AdvertisementApp.UI.Models;
+using AdvertisementApp.UI.ValidationRules;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDependencies(builder.Configuration);
+
+builder.Services.AddTransient<IValidator<UserCreateModel>, UserCreateModelValidator>();
+builder.Services.AddAutoMapper(typeof(UserCreateModelProfile));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        opt.Cookie.Name = "UdemyCookie";
+        opt.Cookie.HttpOnly = true;
+        opt.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+        opt.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+        opt.ExpireTimeSpan = TimeSpan.FromDays(20);
+    });
 
 var app = builder.Build();
 
---
+++ AdvertisementApp.UI/ValidationRules/UserCreateModelValidator.cs
@@ -0,0 +1,33 @@
+﻿using AdvertisementApp.UI.Models;
+using FluentValidation;
+
+namespace AdvertisementApp.UI.ValidationRules
+{
+    public class UserCreateModelValidator : AbstractValidator<UserCreateModel>
+    {
+        //[Obsolete]
+        public UserCreateModelValidator()
+        {
+            //CascadeMode = CascadeMode.StopOnFirstFailure;
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Parola boş olamaz");
+            RuleFor(x => x.Password).MinimumLength(3).WithMessage("Parola min 3 karakter olmalıdır");
+            RuleFor(x => x.Password).Equal(x => x.Password).WithMessage("Parolalar eşleşmiyor");
+            RuleFor(x => x.Firstname).NotEmpty().WithMessage("Ad boş olamaz");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad boş olamaz");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı boş olamaz");
+            RuleFor(x => x.Username).MinimumLength(3).WithMessage("Kullanıcı min 3 karakter olmalıdır");
+            RuleFor(x => new
+            {
+                x.Username,
+                x.Firstname
+            }).Must(x => CanNotFirstName(x.Username, x.Firstname)).WithMessage("kullanıcı adı, adınızı içeremez.").When(x => x.Username != null && x.Firstname != null);
+
+            RuleFor(x => x.GenderId).NotEmpty().WithMessage("Cinsiyet seçimi zorunludur.");
+        }
+
+        private bool CanNotFirstName(string username, string firstname)
+        {
+            return !username.Contains(firstname);
+        }
+    }
+}
